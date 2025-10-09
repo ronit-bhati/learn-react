a5f9e3373e8b89153a0a524fa1d94d85e4189a74
Ronit Bhati <ronitbhati.786@gmail.com>
2025-10-10 03:48:31 +0530

    update

--- 08_react-quiz/src/components/Questions.js
+++ 08_react-quiz/src/components/Questions.js
@@ -1,8 +1,10 @@
 import Options from "./Options";
 
 export default function Questions({ question, dispatch, answer }) {
-  return <div>
-    <h4>{question.question}</h4>
-    <Options question={question} dispatch={dispatch} answer={answer} />
-  </div>
+  return (
+    <div>
+      <h4>{question.question}</h4>
+      <Options question={question} dispatch={dispatch} answer={answer} />
+    </div>
+  );
 }
